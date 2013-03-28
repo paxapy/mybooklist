14492072c5f927fab63cba6c1457fc8711faa82f
Pavel Volkov <paxapy@gmail.com>
2013-03-28 23:46:00 +0400

    plug-in django-pagination

--- mybooklist/settings.py
+++ mybooklist/settings.py
@@ -99,6 +99,18 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pagination.middleware.PaginationMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    "django.core.context_processors.request",
 )
 
 ROOT_URLCONF = 'mybooklist.urls'
@@ -118,8 +130,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'pagination',
     'polls',
 )
+
+PAGINATION_CLEAN_URL = True
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
